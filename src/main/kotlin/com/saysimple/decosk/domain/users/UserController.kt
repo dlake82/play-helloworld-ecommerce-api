a999b17981d7066b8a02a52ed2c59e973d34867d
khw0828 <97077535+gusdn0828@users.noreply.github.com>
2024-04-04 14:01:48 +0900

    feat: 유저 컨트롤러 생성

--- src/main/kotlin/com/saysimple/decosk/domain/users/UserController.kt
+++ src/main/kotlin/com/saysimple/decosk/domain/users/UserController.kt
@@ -1,12 +1,17 @@
+package com.saysimple.decosk.domain.users
+
+import com.saysimple.decosk.domain.users.models.User
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("user")
 class UserController {
     @GetMapping
-    fun get(): User {
+    fun get(): ResponseEntity<User> {
         val user = User(1, "test")
-        return user
+        return ResponseEntity<User>(user, HttpStatus.OK)
     }
 
     @PostMapping
