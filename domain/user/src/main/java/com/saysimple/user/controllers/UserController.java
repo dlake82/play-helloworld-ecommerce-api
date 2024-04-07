bcb42d169104d7423cb8ac36de55a6109d226683
dlake82 <dlake0828@gmail.com>
2024-04-07 21:06:59 +0900

    feat: 디스커버리, 유저 추가

---
+++ domain/user/src/main/java/com/saysimple/user/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.saysimple.user.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+    @GetMapping("/health_check")
+    public String status() {
+        return "I am OK";
+    }
+}
