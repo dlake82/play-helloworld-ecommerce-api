cc7f9dcc6d35cca9c9b9d9f6433319c319d6efcd
dlake82 <dlake0828@gmail.com>
2024-04-12 12:31:32 +0900

    chore: kafka connect lib 추가

--- kafka/connect-install.sh
+++ kafka/connect-install.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-URL="https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-jdbc/versions/10.7.6/confluentinc-kafka-connect-jdbc-10.7.6.zip"
-FILE_NAME="confluentinc-kafka-connect-jdbc-10.7.6.zip"
-TARGET_DIR="./kafka/jars"
+# Kafka Connect JDBC plugin
+CONNECT_URL="https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-jdbc/versions/10.7.6/confluentinc-kafka-connect-jdbc-10.7.6.zip"
+JDBC_FILE_NAMEFILE_NAME="confluentinc-kafka-connect-jdbc-10.7.6.zip"
+TARGET_DIR="./kafka/libs"
 
-curl -L $URL -o $FILE_NAME
+# MariaDB Java client
+MARIA_JAVA_CLIENT_FILE_NAME=mariadb-java-client-3.3.3.jar
+MARIA_JAVA_CLIENT_URL="https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/3.3.3/$MARIA_JAVA_CLIENT_FILE_NAME"
+
+# rm kafka libs
+rm -rf $TARGET_DIR
+
+# Download and extract the Kafka Connect JDBC plugin
 mkdir -p $TARGET_DIR
+curl -L $CONNECT_URL -o $JDBC_FILE_NAMEFILE_NAME
 
-unzip $FILE_NAME
+unzip $JDBC_FILE_NAMEFILE_NAME
 cp -r confluentinc-kafka-connect-jdbc-10.7.6/lib/* $TARGET_DIR
 
-rm $FILE_NAME
+rm $JDBC_FILE_NAMEFILE_NAME
 rm -rf confluentinc-kafka-connect-jdbc-10.7.6
+
+# Download the MariaDB Java client
+echo Download the MariaDB Java client
+
+rm -rf $MARIA_JAVA_CLIENT_FILE_NAME
+
+curl -L "$MARIA_JAVA_CLIENT_URL" -o $TARGET_DIR/$MARIA_JAVA_CLIENT_FILE_NAME
+mv $MARIA_JAVA_CLIENT_FILE_NAME $TARGET_DIR
